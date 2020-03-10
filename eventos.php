7ec9b3ec8f8c3bd9552262f35b4215060ec3a42e
Lehi Quincho <lqmyacs@gmail.com>
2020-03-10 16:30:59 -0500

    gallery -events

--- eventos.php
+++ eventos.php
@@ -17,14 +17,64 @@
             <h3 id="galery-title" class="font-montserrat">GALERIA</h3>     
             <hr id="line-hr" width="60">
             
+            <div class="header" id="myHeader">
+
+
+            <div class="grid-container">
+                <div class="grid-item parent">
+                    <img src="img/galery1.jpeg">
+                </div>
+                <div class="grid-item">
+                    <img src="img/galery2.jpeg">
+                </div>
+                <div class="grid-item">
+                    <img src="img/galery3.jpeg">
+                </div>  
+                <div class="grid-item">
+                    <img src="img/galery4.jpeg">
+                </div>
+                <div class="grid-item">
+                    <img src="img/galery5.jpeg">
+                </div>
+                <div class="grid-item">
+                    <img src="img/galery6.jpeg">
+                </div>   
+            </div>
 
 
         </div>
+
+        <?php include("footer.php"); ?>
     </body>
 </html>
 
 
 <style>
+
+
+.grid-container {
+  display: grid;
+  grid-row-gap: 50px;
+  grid-column-gap: 70px;
+  grid-template-columns: auto auto auto;
+  padding: 150px;
+}
+
+.grid-item {
+  
+  padding: 20px;
+  text-align: center;
+  
+    width: 100%; 
+    height:20rem;
+    overflow: hidden;
+    
+    /* Magic */
+    display: flex;
+    align-items: center; /* vertical */
+    justify-content: center; /* horizontal */
+}
+
 
     #line-hr{
         border: 0.4pt solid #1D4E9B;        
@@ -32,6 +82,7 @@
 
     #galery-title{
         margin-top: 100px;
+        font-size: 33;
         font-weight:bold;
         color: #1D4E9B;
     }
