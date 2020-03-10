4b337222fe2ca07581502bdd303a06ecff2080b2
luis <luisbch9@gmail.com>
2020-03-10 10:42:25 -0500

    incluye un boton + conf finalizado

--- conference.php
+++ conference.php
@@ -8,6 +8,14 @@
         <div class="center" width="100%">
             <img src="img/conference.png" alt="Conferencia" width="70%" >
             <h1 id="csc-title" class="font-lato">Computer Science<br>Education Conference</h1>
+            <p id="p-text" class="font-lato">
+                Que ocurriría si le damos la oportunidad para que 50,000 niños peruanos tengan las herramientas para mejorar el mundo.
+            </p>
+
+            <button class="boton">
+                SABER MÁS
+            </button>
+            
         </div>
     </body>
 </html>
@@ -17,5 +25,12 @@
     #csc-title{
         font-size:35pt;
         font-weight:bold;
+        margin-top: 35pt;
+    }
+
+    #p-text{
+        padding-left: 15% ;
+        padding-right: 15% ;
+        font-size: 16pt;    
     }
 </style>
