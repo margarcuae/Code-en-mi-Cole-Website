fab09ea1613f1b27ac01477378fad1d015f04e4e
luis <luisbch9@gmail.com>
2020-03-10 13:11:34 -0500

    docente avance

--- docente.php
+++ docente.php
@@ -5,11 +5,54 @@
 
     <body>
         <?php include("nav.php"); ?>
-        
+        <div class="center" width="100%">
+            <img src="img/docente.png" alt="Conferencia" width="30%" >
+            <h1 id="csc-title" class="font-lato">Programa de Formación Docente<br> <span style="color:blue"> en Ciencia de la Computación </span> </h1>
+            <p id="p-text" class="font-lato">
+               Buscamos docentes de educación básica regular de nivel inicial, primario y secundario para dictar capacitaciones presenciales sobre Ciencia de la Computación y su pedagogía. La capacitación posee una duración de 120 horas totales, distribuidas en tres meses.
+          
+               <br><br>
+
+               Luego de las capacitaciones se espera la aplicación de los conocimientos adquiridos para medir el impacto en los estudiantes.
+            </p>
+        </div>
+
+        <div id="barra">
+    
+        </div>
+    
+
+
+
+
+       
     </body>
 </html>
 
 
 <style>
+
+    #csc-title{
+        font-size:40pt;
+        font-weight:bold;
+        margin-top: 35pt;
+    }
+
+    #p-text{
+        margin-top: 100px;
+        padding-left: 15% ;
+        padding-right: 15% ;
+        font-size: 16pt;    
+    }
+
+   
+    #barra{
+        width:100%;
+        height:100px;
+        clip-path: polygon(0 0,0 100%,100% 100%,100% 0,62% 0, 50% 30px,38% 0);
+        background-color: #e6e6e6;
+    }
+
+
 
 </style>
