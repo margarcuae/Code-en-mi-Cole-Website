22fc3d953f4541dac155757a9ca35649eff61be9
Lehi Quincho <lqmyacs@gmail.com>
2020-03-10 14:56:47 -0500

    events.php

---
+++ eventos.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+    <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <?php include("nav.php"); ?>
+        <div class="center" width="100%">
+            <img src="img/events.png" alt="Eventos" width="70%" >
+            <h1 id="csc-title" class="font-lato">Encuentro de Organizaciones<br> Educativas Innovadoras</h1>
+            <p id="p-text" class="font-lato">
+            En Latinoamérica y el Perú se vienen desarrollando diferentes iniciativas que impulsan cambios que la <br>
+            educación y la formación necesitan para transformarse en la era digital. El reto que asumimos es convocar<br> estas iniciativas. Conocerlas, integrarlas y fortalecerlas, para construir juntos: una nueva era.            
+            </p>
+
+
+            <h3 id="galery-title" class="font-montserrat">GALERIA</h3>     
+            <hr id="line-hr" width="60">
+            
+
+
+        </div>
+    </body>
+</html>
+
+
+<style>
+
+    #line-hr{
+        border: 0.4pt solid #1D4E9B;        
+    }
+
+    #galery-title{
+        margin-top: 100px;
+        font-weight:bold;
+        color: #1D4E9B;
+    }
+
+    #csc-title{
+        font-size:50pt;
+        font-weight:bold;
+        margin-top: 0;
+    }
+
+    #p-text{
+        margin-top: 100px;
+        padding-left: 15% ;
+        padding-right: 15% ;
+        font-size: 16pt;    
+    }
+
+
+</style>
+
+
+
+
+
