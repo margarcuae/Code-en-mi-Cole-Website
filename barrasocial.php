bd096a224a2a7a9da279e44083e0a714a21b4943
margarcuae <margarcuae@gmail.com>
2020-03-10 16:39:06 -0500

    asdf

--- barrasocial.php
+++ barrasocial.php
@@ -1,14 +1,14 @@
 <div class="social-bar">
-    <a href="https://www.facebook.com/DevCode.la" class="icon icon-facebook2" target="_blank">
+    <a href="" class="icon icon-facebook2" target="_blank">
     <img src="logos/facebook2.png" alt="" width="18px" class="arreglando-f">
 </a>
-    <a href="https://twitter.com/DevCodela" class="icon icon-twitter" target="_blank">
+    <a href="" class="icon icon-twitter" target="_blank">
     <img src="logos/youtube2.png" alt="" width="25px">
 </a>
-    <a href="https://www.youtube.com/c/devcodela" class="icon icon-youtube" target="_blank">
+    <a href="" class="icon icon-youtube" target="_blank">
     <img src="logos/instagram2.png" alt="" width="25px">
 </a>
-    <a href="https://www.instagram.com/devcodela/" class="icon icon-instagram" target="_blank">
+    <a href="" class="icon icon-instagram" target="_blank">
     <img src="logos/tuiter2.png" alt="" width="25px">
 </a>
   </div>
