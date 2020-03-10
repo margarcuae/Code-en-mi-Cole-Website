e3db23ccf397a776049eac2fccf23a7e169cfc2f
luis <luisbch9@gmail.com>
2020-03-10 10:24:48 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ conference.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+    <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <?php include("nav.php"); ?>
+        <div class="center" width="100%">
+            <img src="img/conference.png" alt="Conferencia" width="70%" >
+            <h1 id="csc-title" class="font-lato">Computer Science<br>Education Conference</h1>
+        </div>
+    </body>
+</html>
+
+
+<style>
+    #csc-title{
+        font-size:35pt;
+        font-weight:bold;
+    }
+</style>
--- head.php
+++ head.php
@@ -2,6 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>CEMC</title>
 
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat|Lato&display=swap" rel="stylesheet">
 
 <link rel="stylesheet" href="css/main.css">
