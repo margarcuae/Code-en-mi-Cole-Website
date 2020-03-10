e3cb29639ceb716ff6ab0b30fccc0fb52dbf9678
margarcuae <margarcuae@gmail.com>
2020-03-10 16:26:14 -0500

    fix icon

--- barrasocial.php
+++ barrasocial.php
@@ -1,5 +1,5 @@
 <div class="social-bar">
-    <a href="https://www.facebook.com/DevCode.la" class="icon icon-facebook" target="_blank">
+    <a href="https://www.facebook.com/DevCode.la" class="icon icon-facebook2" target="_blank">
     <img src="logos/facebook2.png" alt="" width="25px" >
 </a>
     <a href="https://twitter.com/DevCodela" class="icon icon-twitter" target="_blank">
