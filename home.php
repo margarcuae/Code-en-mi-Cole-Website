c491efc3b45c1fa13fdcfeafb09e3a8d169f51a4
margarcuae <margarcuae@gmail.com>
2020-03-11 16:29:02 -0500

    home parte 4 avance

--- home.php
+++ home.php
@@ -15,14 +15,12 @@
     <h2>¿QUÉ OCURRE?</h2>
     </div>
     <div class="home-foto1">
-        FOTO 1
+        <img src="img/home/home-recurso4.png" alt="" width="200px">
     </div>
     <div class="home-foto2">
-        FOTO 2
-    </div>
+    <img src="img/home/home-recurso5.png" alt="" width="200px">    </div>
     <div class="home-foto3">
-        FOTO 3
-    </div>
+    <img src="img/home/home-recurso6.png" alt="" width="200px">    </div>
 </section>
 
 
@@ -43,8 +41,66 @@
     </div>
 </section>
 
-
 <section class="home4">
-    
+
+
+    <section class="home4-1">
+        <div class="h4Title">
+            <h2 class="font-montserrat">¿CÓMO LO HACEMOS?</h2>
+        </div>
+
+        <div class="h4Cont1 h4blanco">
+            <div class="h4img1"></div>
+            <div>
+                <p>&#60;Colegios&#62;</p>
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="h4Cont2 h4blanco">
+        <div class="h4img2"></div>
+            <div>
+                <p>&#60;Adecademia&#62;</p>
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="h4Cont3 h4blanco">
+        <div class="h4img3"></div>
+            <div>
+                <p>&#60;Workshops&#62;</p>
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="home4-2">
+        <div class="h4Cont4 h4blanco">
+        <div class="h4img4"></div>
+            <div>
+                <p>&#60;Programas Sociales&#62;</p>
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="h4Cont5 h4blanco">
+        <div class="h4img5"></div>
+
+            <div>
+                <p>&#60;Programa de formación de docente&#62;</p>
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 
 </section>
