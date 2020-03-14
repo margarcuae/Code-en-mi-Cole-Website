a4cf7395d8e181ef29a30f2895ffbef87f3785ac
luis <luisbch9@gmail.com>
2020-03-14 13:31:35 -0500

    academia fin

--- academia.php
+++ academia.php
@@ -1,70 +1,114 @@
 <html>
-    <head>
+
+<head>
     <?php include("head.php"); ?>
-    </head>
+</head>
 
-    <body>
-        <?php include("nav.php"); ?>
-        <div class="font-lato" id="seccion1">
-            <h1>Academia</h1>
-            <p id="navega">Qué hacemos ?    <span style="margin: 0 30px;">></span>     Academia</p>
+<body>
+    <?php include("nav.php"); ?>
+    <div class="font-lato" id="seccion1">
+        <h1>Academia</h1>
+        <p id="navega">Qué hacemos ? <span style="margin: 0 30px;">></span> Academia</p>
+    </div>
+
+    <div id="seccion2" class="font-lato">
+        <div class="titulos-peque"><img src="img/academia/conoce.png" alt="">
+            <h2>CONOCE</h2>
+        </div>
+        <div class="titulos-peque"><img src="img/academia/aprende.png" alt="">
+            <h2>APRENDE</h2>
+        </div>
+        <div class="titulos-peque"><img src="img/academia/crea.png" alt="">
+            <h2>CREA</h2>
+        </div>
+    </div>
+    <p class="font-lato" style="color: #999999; margin: 40px 15%; text-align: center; font-size: 1.4rem;">
+        Con nuestros programas especializados buscamos potenciar las habilidades creativas de nuestros estudiantes
+        convirtiendo sus ideas en proyectos tecnológicos.
+    </p>
+
+    <div id="seccion3">
+        <div style="grid-area: foto1;"><img src="img/academia/foto1.png" class="center-cropped" alt=""></div>
+        <div style="grid-area: foto2;"><img src="img/academia/foto2.png" class="center-cropped" alt=""></div>
+        <div style="grid-area: foto3;"><img src="img/academia/foto3.png" class="center-cropped" alt=""></div>
+
+        <div style="grid-area: texto1;">
+            <h2 class="font-montserrat titulo-sec3">PENSAMIENTO COMPUTACIONAL</h2>
+            <div class="font-lato" style="margin-bottom: 20px;">
+                <h3>Edad Sugerida:</h3>
+                <p class="acompana">De 4 a 7 años</p>
+                <p class="texto">Se trata de la alfabetización digital, y que como tal hay que comenzar desde las primeras etapas del desarrollo individual, al igual como sucede con otras habilidades clave: la lectura y las habilidades matemáticas.</p>
+                <h3>Duración:</h3>
+                <p class="acompana">12 sesiones</p> <br>
+                <p class="acompana" style="font-size: 0.8rem;">150 minutos cada sesión</p>
+            </div>
+            <button class="boton">DESCARGAR PDF</button>
         </div>
 
-        <div id="seccion2" class="font-lato">
-            <div class="titulos-peque"><img src="img/academia/conoce.png" alt=""><h2>CONOCE</h2></div>
-            <div class="titulos-peque"><img src="img/academia/aprende.png" alt=""><h2>APRENDE</h2></div>
-            <div class="titulos-peque"><img src="img/academia/crea.png" alt=""><h2>CREA</h2></div>
+        <div style="grid-area: texto2;">
+            <h2 class="font-montserrat titulo-sec3">DISEÑO Y CREACIÓN DE VIDEOJUEGOS</h2>
+            <div class="font-lato" style="margin-bottom: 20px;">
+                <h3>Edad Sugerida:</h3>
+                <p class="acompana">De 8 a 11 años</p><br>
+                <p class="acompana"> <span style="margin-left: 138px;"></span>   De 12 a 16 años</p>
+                <p class="texto">Permite aprender a programar, de una manera dinámica y ofrece un producto que a agrada a los más jóvenes, dando un gran porcentaje de que usen nuestra enseñanza como base para crear videojuegos con mayor dificultad o guiarse a otras ramas de la ciencia de la computación.</p>
+                <h3>Duración:</h3>
+                <p class="acompana">12 sesiones</p> <br>
+                <p class="acompana" style="font-size: 0.8rem;">150 minutos cada sesión</p>
+            </div>
+            <button class="boton">DESCARGAR PDF</button>
         </div>
-        <p class="font-lato" style="color: #999999; margin: 40px 15%; text-align: center; font-size: 1.4rem;">
-            Con nuestros programas especializados buscamos potenciar las habilidades creativas de nuestros estudiantes convirtiendo sus ideas en proyectos tecnológicos.
-        </p>
+        <div style="grid-area: texto3;">
+            <h2 class="font-montserrat titulo-sec3">CREACIÓN DE APLICACIONES</h2>
+            <div class="font-lato" style="margin-bottom: 20px;">
+                <h3>Edad Sugerida:</h3>
+                <p class="acompana">De 12 a 16 años</p>
+                <p class="texto">El curso enseña a programar aplicaciones móviles desde las más básicas hata las más sofisticadas, con utilización de sensores, gestión de datos y conexión a Internet mediante programación por bloques, además, publicar tus aplicaciones en Google Play</p>
+                <h3>Duración:</h3>
+                <p class="acompana">12 sesiones</p> <br>
+                <p class="acompana" style="font-size: 0.8rem;">150 minutos cada sesión</p>
+            </div>
+            <button class="boton">DESCARGAR PDF</button>
+        </div>
+        
+    </div>
 
-        <div id="seccion3">
-            <div style="grid-area: foto1;"><img src="img/academia/foto1.png" class="center-cropped" alt=""></div>
-            <div style="grid-area: foto2;"><img src="img/academia/foto2.png" class="center-cropped" alt=""></div>
-            <div style="grid-area: foto3;"><img src="img/academia/foto3.png" class="center-cropped" alt=""></div>
-            <div style="grid-area: texto1;">
-                <h2 class="font-lato titulo-sec3">PENSAMIENTO COMPUTACIONAL</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quaerat facilis quasi cum, necessitatibus consequuntur neque hic, quas eum nemo aperiam ipsum fugit est ab. Itaque fuga quas consequatur ab!</p>
-                <button class="boton">DESCARGAR PDF</button>
-            </div>
-            <div style="grid-area: texto2;">
-                <h2 class="font-lato titulo-sec3">PENSAMIENTO COMPUTACIONAL</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quaerat facilis quasi cum, necessitatibus consequuntur neque hic, quas eum nemo aperiam ipsum fugit est ab. Itaque fuga quas consequatur ab!</p>
-                <button class="boton">DESCARGAR PDF</button>
-            </div>
-            <div style="grid-area: texto3;">
-                <h2 class="font-lato titulo-sec3">PENSAMIENTO COMPUTACIONAL</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quaerat facilis quasi cum, necessitatibus consequuntur neque hic, quas eum nemo aperiam ipsum fugit est ab. Itaque fuga quas consequatur ab!</p>
-                <button class="boton">DESCARGAR PDF</button>
-            </div>
+    <div id="seccion4" class="font-montserrat">
+        <div>
+            <p style="font-size: 1.4rem ;">Te gustaría recibir un taller de inducción gratuito? <br> Escríbenos y nos pondremos en contacto contigo.</p>
+            <img src="img/academia/programador.png" alt="" height="300px">
         </div>
+        <div>
+            <div id="cuadrito">
+                <p style="font-size: 1rem; font-weight: bold; padding-top: 25px; margin:0"> 
+                    PREINSCRÍBETE A LA ACADEMIA
+                </p>
+                <iframe src="" frameborder="0">
 
-        <div id="seccion4" class="font-montserrat">
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat iusto impedit at veniam reiciendis eum totam aperiam et repellendus ipsa accusantium nam minus, deserunt quod voluptates ea earum. Sint?</p>
-                <img src="img/academia/programador.png" alt="" height="300px">
-            </div>
-            <div>
-                Cuadrito
+                    
+                </iframe>
             </div>
         </div>
-        <?php include('footer.php'); ?>
-    </body>
+    </div>
+    <?php include('footer.php'); ?>
+</body>
+
 </html>
 
 <style>
-    h1{
+    h1 {
         font-size: 4.5rem;
     }
-    #navega{
+
+    #navega {
         position: absolute;
         margin: 0;
         bottom: 20px;
         font-size: 1.1rem;
-        font-weight:normal;
+        font-weight: normal;
     }
-    #seccion1{
+
+    #seccion1 {
         position: relative;
         padding-top: 30px;
         padding-left: 55px;
@@ -72,55 +116,57 @@
         height: 250px;
         background-image: url('img/academia/background.png');
         background-repeat: no-repeat;
-        color:white;
+        color: white;
     }
 
-    #seccion2{
-        
+    #seccion2 {
+
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
-        margin:  100px 10% 0 10%;
+        margin: 100px 10% 0 10%;
         column-gap: 50px;
         justify-items: center;
         flex-direction: column;
         text-align: center;
     }
 
-    .titulos-peque{
+    .titulos-peque {
         font-size: 0.9rem;
         color: #0066FF;
     }
 
-    #seccion3{
+    #seccion3 {
         background-color: #e5e5e5;
         display: grid;
-        grid-template-areas: 
-        'foto1 texto1'
-        'foto2 texto2'
-        'foto3 texto3';
+        grid-template-areas:
+            'foto1 texto1'
+            'foto2 texto2'
+            'foto3 texto3';
         grid-template-columns: 1fr 1fr;
         justify-items: center;
         text-align: center;
         padding: 100px 10%;
         row-gap: 100px;
-        column-gap: 30px;
+        column-gap: 60px;
     }
 
-    .titulo-sec3{
+    .titulo-sec3 {
         font-size: 1.4rem;
         font-weight: bold;
         color: #3bb8ad;
     }
 
     .center-cropped {
-        object-fit: none; /* Do not scale the image */
-  object-position: center; /* Center the image within the element */
-  height: 300px;
-  width: 400px;
+        object-fit: none;
+        /* Do not scale the image */
+        object-position: center;
+        /* Center the image within the element */
+        height: 300px;
+        width: 400px;
     }
 
-    #seccion4{
-        
+    #seccion4 {
+
         display: grid;
         grid-template-columns: 1fr 1fr;
         margin: 30px 10%;
@@ -129,5 +175,32 @@
         justify-items: center;
         text-align: center;
     }
+
+    h3{
+        display: inline;
+        color: #666666;
+    }
+    
+    .acompana{
+        display: inline;
+        color: #666666;
+    }
+
+    .texto{
+        color:#4d4d4d;
+        text-align: justify;
+        text-justify: inter-word;
+    }
+
+    .boton{
+        background-color: #58bbc9;
+    }
+
+    #cuadrito{
+        height: 400px;
+        width: 400px;
+        -webkit-box-shadow: 13px 17px 31px -4px rgba(0,0,0,0.55);
+        -moz-box-shadow: 13px 17px 31px -4px rgba(0,0,0,0.55);
+        box-shadow: 13px 17px 31px -4px rgba(0,0,0,0.55);
     }
 </style>
