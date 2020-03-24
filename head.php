8bec478874aa253efe7680be74522eb0d1bdf038
Lehi Quincho <lehi_10@hotmail.com>
2020-03-23 20:12:15 -0500

    Style Blog

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>CEMC</title>
+<title>Code En Mi Cole</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 
