724d9ff56411b2ba764ccb44f2c9efcbaa1b20a9
luis <luisbch9@gmail.com>
2020-03-25 12:46:53 -0500

    Modal

--- modal.php
+++ modal.php
@@ -1,38 +1,35 @@
-<div class="overlay" id="overlay-modal">
-			<div class="popup" id="popup-modal">
-				<a href="#" id="btn-cerrar-popup-modal" class="btn-cerrar-popup"><i class="fas fa-times"></i></a>
-          
+<div class="overlay font-lato" id="overlay-modal">
+    <div class="popup" id="popup-modal">
+        <a href="#" id="btn-cerrar-popup-modal" class="btn-cerrar-popup"><i class="fas fa-times"></i></a>
 
+        <h2>
+            <?php the_title() ?>
+        </h2>
 
-<h2>
-        <?php the_title() ?>
-    </h2>
-    
-    <br>
-    date: <?php the_date() ?>
-    <br>
-    autor: <?php the_author(); ?>
-    <br>    
+        Fecha: <?php the_date() ?>
+        <br>
+        Autor: <?php the_author(); ?>
+        <br>
+        <?php  the_content(); ?>
 
-			</div>
     </div>
-    
+</div>
 
 
     <script>
 
-var	overlayModal = document.getElementById('overlay-modal'),
-	popupModal = document.getElementById('popup-modal'),
-	btnCerrarPopupModal = document.getElementById('btn-cerrar-popup-modal');
+        var overlayModal = document.getElementById('overlay-modal'),
+            popupModal = document.getElementById('popup-modal'),
+            btnCerrarPopupModal = document.getElementById('btn-cerrar-popup-modal');
 
 
-	overlayModal.classList.add('active');
-	popupModal.classList.add('active');
+        overlayModal.classList.add('active');
+        popupModal.classList.add('active');
 
-btnCerrarPopupModal.addEventListener('click', function(e){
-	e.preventDefault();
-	overlayModal.classList.remove('active');
-	popupModal.classList.remove('active');
-});
+        btnCerrarPopupModal.addEventListener('click', function (e) {
+            e.preventDefault();
+            overlayModal.classList.remove('active');
+            popupModal.classList.remove('active');
+        });
 
     </script>
