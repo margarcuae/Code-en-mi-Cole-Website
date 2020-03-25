2812c810543efe2e7865cf772685df4d7c4f446a
margarcuae <margarcuae@gmail.com>
2020-03-25 12:04:17 -0500

    fix2

--- head.php
+++ head.php
@@ -5,6 +5,9 @@
 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 
 
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
 <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/main.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.4/gsap.min.js"></script>
 
