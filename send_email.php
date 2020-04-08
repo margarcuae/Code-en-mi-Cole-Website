d7511bf78fff3d1c31d4fd6ac0e3d8dcb1dfd55b
Lehi Quincho <lehi_10@hotmail.com>
2020-04-08 09:06:06 -0500

    fix error

--- send_email.php
+++ send_email.php
@@ -6,7 +6,7 @@ $headers = "From: lqmyacs@gmail.com" . "\r\n";
 //."CC: somebodyelse@example.com";
 
 // Sending email
-if(mail($to,$subject,$txt,$headers);){
+if(mail($to,$subject,$txt,$headers)){
     echo 'Your mail has been sent successfully.';
 } else{
     echo 'Unable to send email. Please try again.';
