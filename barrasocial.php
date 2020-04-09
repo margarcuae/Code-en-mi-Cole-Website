df2bce01c5a286d19a54123a0faa91549eb408e0
Margar <margarcuae@gmail.com>
2020-04-09 15:53:59 -0500

    redes sociales links

--- barrasocial.php
+++ barrasocial.php
@@ -1,14 +1,14 @@
 <div class="social-bar">
-    <a href="" class="icon icon-facebook2" target="_blank">
+    <a href="https://www.facebook.com/Code.en.mi.Cole/" class="icon icon-facebook2" target="_blank">
     <img src="<?php echo get_bloginfo('template_directory'); ?>/logos/facebook2.png" alt="" width="18px" class="arreglando-f">
 </a>
-    <a href="" class="icon icon-twitter" target="_blank">
+    <a href="https://www.youtube.com/channel/UCC9baQw3C5ayFIsTw-TCAjw" class="icon icon-twitter" target="_blank">
     <img src="<?php echo get_bloginfo('template_directory'); ?>/logos/youtube2.png" alt="" width="25px">
 </a>
-    <a href="" class="icon icon-youtube" target="_blank">
+    <a href="https://www.instagram.com/code.en.mi.cole/" class="icon icon-youtube" target="_blank">
     <img src="<?php echo get_bloginfo('template_directory'); ?>/logos/instagram2.png" alt="" width="25px">
 </a>
-    <a href="" class="icon icon-instagram" target="_blank">
+    <a href="https://twitter.com/codenmicole?lang=es" class="icon icon-instagram" target="_blank">
     <img src="<?php echo get_bloginfo('template_directory'); ?>/logos/tuiter2.png" alt="" width="25px">
 </a>
   </div>
